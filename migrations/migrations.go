1fc1f3822ab5cbfd7ac92b4702d9939dabdac59d
Frankie Primerano <frankieprimerano@gmail.com>
2024-07-11 11:05:44 -0400

    fixed ignore

---
+++ migrations/migrations.go
@@ -0,0 +1,220 @@
+package migrations
+
+import (
+	"fmt"
+	"log"
+        "morph/core"
+)
+
+/* 
+
+UNIVERSAL STRUCTURES
+
+- Roles
+- Tablespaces
+
+*/
+
+func perr(err error) {
+  core.Perr(err)
+}
+
+func buildStringArray[T any](vals []T, f func(T) string) []string {
+  var result []string
+
+  for _, v := range vals {
+    result = append(result, f(v))
+  }
+
+  return result
+}
+
+func computeAddedObjects(d Diffable) []string {
+  var added_items []string
+
+  for _, n := range d.GetNewNames() {
+    found := false
+    for _, c := range d.GetCurrentNames() {
+      if c == n {
+        found = true
+        break
+      }
+    }
+
+    if !found {
+      added_items = append(added_items, n)
+    }
+  }
+
+  return added_items 
+}
+
+func computeRemovedObjects(d Diffable) []string {
+  var removed_items []string
+
+  for _, n := range d.GetCurrentNames() {
+    found := false
+    for _, c := range d.GetNewNames() {
+      if c == n {
+        found = true
+        break
+      }
+    }
+
+    if !found {
+      removed_items = append(removed_items, n)
+    }
+  }
+
+  return removed_items 
+}
+
+type Diffable interface {
+  GenerateDropStmts(ctx *core.Context) []string
+  GenerateAddStmts(ctx *core.Context) []string
+  GenerateUpdateStmts(ctx *core.Context) []string
+  GetCurrentNames() []string
+  GetNewNames() []string
+}
+
+func buildMigrationsFor(ctx *core.Context, x Diffable) []string {
+  var migrations []string
+
+  migrations = append(migrations, x.GenerateDropStmts(ctx)...)
+  migrations = append(migrations, x.GenerateAddStmts(ctx)...)
+  migrations = append(migrations, x.GenerateUpdateStmts(ctx)...)
+
+  return migrations
+}
+
+
+func generateExtensionMigrations(ctx *core.Context) []string {
+  list_current := get_list_of_extensions(ctx.Db)
+  list_new := get_list_of_extensions(ctx.MigrationDb)
+
+  diffable_exts := DiffableExtensions {
+    current_extensions: list_current,
+    new_extensions: list_new,
+  }
+
+  return buildMigrationsFor(ctx, &diffable_exts)
+}
+
+func generateSchemaMigrations(ctx *core.Context) []string {
+  list_current := get_list_of_schemas(ctx.Db)
+  list_new := get_list_of_schemas(ctx.MigrationDb)
+
+  diffable_schemas := DiffableSchema {
+    current_schemas: list_current,
+    new_schemas: list_new,
+  }
+
+  return buildMigrationsFor(ctx, &diffable_schemas)
+}
+
+func generateRoleMigrations(ctx *core.Context) []string {
+  var roles []string
+  return roles
+}
+
+func generateSequenceMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateTableMigrations(ctx *core.Context) []string {
+  list_current := get_list_of_tables(ctx.Db)
+  list_new := get_list_of_tables(ctx.MigrationDb)
+
+  current_map := make(map[string]*PgTable)
+  new_map := make(map[string]*PgTable)
+
+  for _, c := range list_current {
+    current_map[c.relnamespace + "." + c.relname] = &c
+  }
+
+  for _, c := range list_new {
+    new_map[c.relnamespace + "." + c.relname] = &c
+  }
+
+  diffable_tables := DiffableTable {
+    current_tables: list_current,
+    new_tables: list_new,
+    current_tables_map: current_map,
+    new_tables_map: new_map,
+  }
+
+  return buildMigrationsFor(ctx, &diffable_tables)
+}
+
+func generateColumnsMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateIndexesMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateViewsMigrations(ctx *core.Context) []string {
+  var migrations []string
+
+  //list_current := get_list_of_views(ctx.db)
+  //list_new := get_list_of_views(ctx.migration_db)
+
+  return migrations
+}
+
+func generateMatviewsMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateForeignKeysMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateFunctionsMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateTriggersMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateOwnersMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateGrantRelationshipsMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func generateGrantAttributesMigrations(ctx *core.Context) []string {
+  var migrations []string
+  return migrations
+}
+
+func GenerateMigrations(ctx *core.Context) []string {
+  var migrations []string
+
+  log.Println("Generating migrations...")
+
+  migrations = append(migrations, generateExtensionMigrations(ctx)...)
+  migrations = append(migrations, generateSchemaMigrations(ctx)...)
+  // migrations = append(migrations, generate_role_migrations(ctx)...)
+  migrations = append(migrations, generateTableMigrations(ctx)...)
+
+  fmt.Println("MIGRATIONS:")
+  for _, m := range migrations {
+    fmt.Printf("%v\n", m)
+  }
+
+  return migrations
+}
---
+++ migrations/role_migrations.go
@@ -0,0 +1,131 @@
+package migrations
+
+import (
+	"database/sql"
+        "morph/core"
+	"fmt"
+)
+
+type DiffableRoles struct {
+  current_roles []PgRole
+  new_roles []PgRole
+  current_roles_map map[string]*PgRole
+  new_roles_map map[string]*PgRole
+}
+
+type PgRole struct {
+  rolname string
+  rolsuper bool
+  rolinherit bool
+  rolcreaterole bool
+  rolcreatedb bool
+  rolcanlogin bool
+  rolreplication bool
+  rolconnlimit int
+  rolbypassrls bool 
+}
+
+func (x *DiffableRoles) generateAlterStmtsFor(name string) []string {
+  var stmts []string
+
+  fmt.Printf("x.new_roles_map[name]: %v\n", x.new_roles_map[name])
+
+  return stmts
+}
+
+func (x *DiffableRoles) GetCurrentNames() []string {
+  return buildStringArray[PgRole](x.current_roles, func(r PgRole) string {
+    return r.rolname
+  })
+}
+
+func (x *DiffableRoles) GetNewNames() []string {
+  return buildStringArray[PgRole](x.new_roles, func(r PgRole) string {
+    return r.rolname
+  })
+}
+
+func (x *DiffableRoles) GenerateAddStmts(ctx *core.Context) []string {
+  added_roles := computeAddedObjects(x)
+  added := buildStringArray[string](added_roles, func(r string) string {
+    return fmt.Sprintf("CREATE ROLE %s;", r)
+  })
+
+  for _, role := range added_roles {
+    x.generateAlterStmtsFor(role)
+  }
+
+  return added
+}
+
+func (x *DiffableRoles) GenerateDropStmts(ctx *core.Context) []string {
+  return buildStringArray[string](computeRemovedObjects(x), func(r string) string {
+    return fmt.Sprintf("DROP ROLE IF EXISTS %s;", r);
+  })
+}
+
+func (x *DiffableRoles) GenerateUpdateStmts(ctx *core.Context) []string {
+  var updated []string
+  return updated
+}
+
+func getListOfRoles(db *sql.DB) []PgRole {
+  var extensions []PgRole
+  r, err := db.Query(`
+    select 
+      rolname,
+      rolsuper,
+      rolinherit,
+      rolcreaterole,
+      rolcreatedb,
+      rolcanlogin,
+      rolreplication,
+      rolconnlimit,
+      rolbypassrls
+    from pg_catalog.pg_roles
+  `)
+
+  defer r.Close()
+
+  perr(err)
+
+  for r.Next() {
+    var rolname string
+    var rolsuper bool
+    var rolinherit bool
+    var rolcreaterole bool
+    var rolcreatedb bool 
+    var rolcanlogin bool 
+    var rolreplication bool
+    var rolconnlimit int
+    var rolbypassrls bool
+
+    r.Scan(
+      &rolname,
+      &rolsuper,
+      &rolinherit,
+      &rolcreatedb,
+      &rolcreatedb,
+      &rolcanlogin,
+      &rolreplication,
+      &rolconnlimit,
+      &rolbypassrls,
+    )
+
+    extensions = append(extensions, PgRole { 
+      rolname: rolname,
+      rolsuper: rolsuper,
+      rolinherit: rolinherit,
+      rolcreaterole: rolcreaterole,
+      rolcreatedb: rolcreatedb,
+      rolcanlogin: rolcanlogin,
+      rolreplication: rolreplication,
+      rolconnlimit: rolconnlimit,
+      rolbypassrls: rolbypassrls,
+    })
+
+    fmt.Printf("extensions: %v\n", extensions)
+  }
+
+  return extensions
+}
