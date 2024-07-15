95b38edf214f4862bf85e9b102cc135f88d59aa5
Frankie Primerano <frankieprimerano@gmail.com>
2024-07-14 20:01:21 -0400

    Readme

--- test_migrations/0000.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE person (id serial PRIMARY KEY);
-CREATE TABLE person_name (person_id bigint REFERENCES person (id) NOT NULL, name text);
--- test_migrations/0001.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE person ADD COLUMN created timestamp default now();
-ALTER TABLE person_name ADD COLUMN created timestamp default now();
